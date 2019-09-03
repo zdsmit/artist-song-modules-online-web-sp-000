72a1769b46c0f48e688743af195a4b71e7b20f46
Zach  Smith <zdsmith001@gmail.com>
2019-09-03 15:14:44 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 require 'pry'
 
 # require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
+ require_relative '../lib/concerns/memorable'
 # require_relative '../lib/concerns/paramable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
+
+#Left off at 'Advanced: The find_by_name Method'
---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,11 @@
+module Memorable
+
+  def reset_all
+    all.clear
+  end
+
+  def count
+    all.count
+  end
+
+end
